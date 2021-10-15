1ed8456894d3cac5ae5325876264ecafd648cc42
James Hodgkinson <james@terminaloutcomes.com>
2021-10-15 19:21:50 +1000

    adding grabipv6

---
+++ examples/grabipv6.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+""" pulls and lists the IPv6 addresess for your services """
+
+from ipaddress import ip_network,  IPv4Network, IPv6Network
+
+from aussiebb import AussieBB
+from loguru import logger
+
+from config import USERNAME, PASSWORD
+
+client = AussieBB(USERNAME, PASSWORD)
+
+logger.debug(client.login())
+
+
+services = client.get_services()
+found_networks = []
+for service in services:
+    logger.debug(service)
+    if 'ipAddresses' in service:
+        logger.info("Found a service: {}", service.get('description'))
+        for address in service.get("ipAddresses"):
+            logger.debug("address: {}", address)
+            try:
+                parsed = ip_network(address)
+            except Exception as error_message: # pylint: disable=broad-except
+                logger.error("Not sure what this was, but it's not an address! {} - {}", address, error_message)
+                continue
+
+            if isinstance(parsed, IPv4Network):
+                logger.debug("Found IPv4, nobody cares about this old stuff!")
+            elif isinstance(parsed, IPv6Network):
+                logger.debug("Found IPv6, woo! {}", parsed)
+                found_networks.append(parsed)
+logger.info("Found the following IPv6 networks:")
+for network in found_networks:
+    logger.info(" - {}", network)
